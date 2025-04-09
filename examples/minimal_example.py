676a2600c3a027e7ee49ab268f5595838189b666
Arthur Masson <arthur.masson@inria.fr>
2025-04-09 11:55:34 +0200

    create docs deps group for github action

--- examples/minimal_example.py
+++ examples/minimal_example.py
@@ -1,4 +1,3 @@
-
 from cema.environment_manager import EnvironmentManager
 
 # Initialize the environment manager
@@ -7,13 +6,13 @@ from cema.environment_manager import EnvironmentManager
 environmentManager = EnvironmentManager("micromamba/")
 
 # Create and launch an isolated Conda environment named "numpy"
-env = environmentManager.create("numpy", {"pip":["numpy==2.2.4"]})
+env = environmentManager.create("numpy", {"pip": ["numpy==2.2.4"]})
 env.launch()
 
 # Import example_module in the environment, see example_module.py below
 minimal_module = env.importModule("minimal_module.py")
 # example_module is a proxy to example_module.py in the environment
-array = [1,2,3]
+array = [1, 2, 3]
 result = minimal_module.sum(array)
 
 print(f"Sum of {array} is {result}.")
